fada967512cd9e39490a9c9d45ad9a4054a0cb12
Hamid <hamidsoftdev@GMAIL.COM>
2022-04-14 01:15:49 +0430

    Initial commit

---
+++ zango_app/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ZangoAppConfig(AppConfig):
+    name = 'zango_app'
---
+++ zango_app/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+import os
+
+# Create your views here.
+def home(request):
+    return render(request, 'index.html', {'what':'Django File Upload'})
+
+def upload(request):
+    if request.method == 'POST':
+        handle_uploaded_file(request.FILES['file'], str(request.FILES['file']))
+        return HttpResponse("Successful")
+
+    return HttpResponse("Failed")
+
+def handle_uploaded_file(file, filename):
+    if not os.path.exists('upload/'):
+        os.mkdir('upload/')
+
+    with open('upload/' + filename, 'wb+') as destination:
+        for chunk in file.chunks():
+            destination.write(chunk)
